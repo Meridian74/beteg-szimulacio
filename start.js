32bec247bf31491cd123743a26a4d6b4782574d3
meridian74 <meridian@freemail.hu>
2020-10-31 20:28:04 +0100

    módosítások

--- start.js
+++ start.js
@@ -1,33 +1,48 @@
 // PARAMÉTEREK:
-SCREEN_W = 800;
-SCREEN_H = 600;
-NUM_OF_POPULATION = 100000;
-CHANCE_OF_SICK = 5.00; // kezdeti beteg egyedek %-os aránya az össz populációban
+const SCREEN_W = 1120;
+const SCREEN_H = 630;
+const CHANCE_OF_SICK = 5.00;  // kezdeti beteg egyedek %-os aránya az össz populációban
+const N = 500;                // populáció létszáma
 
-var individuals = new Array(NUM_OF_POPULATION);
+var m = 1000;                 // M lépés szám
+
+var beta = 10.00;
+var gamma = 10.00;
+var delta = 10.00;
+var population = new Array();
 
 // --- INICIALIZÁLÁS ---
 function setup() {
    frameRate(60);
    createCanvas(SCREEN_W, SCREEN_H);
-   populationInit(NUM_OF_POPULATION);
+   populationInit();
 }
 
 // --- FŐPROGRAM ---
 function draw() {
    background(10);
-   
+   drawAxes();
 
 }
 
-function populationInit(quantity) {
-   for (let i = 0; i < quantity; i++) {
-      let status = random();
+function populationInit() {
+   for (let i = 0; i < N; i++) {
+      let status = random(100);
       if (status < CHANCE_OF_SICK)
-         status = 1;
+         population[i] = 1;
       else
-         status = 0;
-      individuals[i] = new Person(status);
-   } 
-   
+         population[i] = 0;
+   }
+   console.log(population.length);
+}
+
+function drawAxes() {
+   stroke(255, 234, 0);
+   strokeWeight(3);
+   line(60,70,60,590);
+   line(50,70,60,70);
+   line(50,320,60,320);
+   line(40,570,1060,570);
+   line(1060,570,1060,580);
+   line(560,570,560,580);
 }
