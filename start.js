3d2bf4f56ded0f43d47799bb57692d6e3960aada
meridian74 <meridian@freemail.hu>
2020-10-31 16:16:23 +0100

    first step

--- start.js
+++ start.js
@@ -1,7 +1,33 @@
+// PARAMÉTEREK:
+SCREEN_W = 800;
+SCREEN_H = 600;
+NUM_OF_POPULATION = 100000;
+CHANCE_OF_SICK = 5.00; // kezdeti beteg egyedek %-os aránya az össz populációban
+
+var individuals = new Array(NUM_OF_POPULATION);
+
+// --- INICIALIZÁLÁS ---
 function setup() {
-  // put setup code here
+   frameRate(60);
+   createCanvas(SCREEN_W, SCREEN_H);
+   populationInit(NUM_OF_POPULATION);
 }
 
+// --- FŐPROGRAM ---
 function draw() {
-  // put drawing code here
+   background(10);
+   
+
+}
+
+function populationInit(quantity) {
+   for (let i = 0; i < quantity; i++) {
+      let status = random();
+      if (status < CHANCE_OF_SICK)
+         status = 1;
+      else
+         status = 0;
+      individuals[i] = new Person(status);
+   } 
+   
 }
